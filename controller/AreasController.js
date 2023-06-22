22474849729f6a81deea7a6466e4e2e47ef81a63
rizwar <rizwar.adnanbyantara@gmail.com>
2023-06-22 15:02:00 +0700

    Perbaikan BaseResponse

--- controller/AreasController.js
+++ controller/AreasController.js
@@ -8,7 +8,7 @@ const getAll = async (req, res) => {
         if (data === null) {
             return res.status(400).json({ code: 400, message: 'Data Not Found' })
         }
-        const response_data = BaseResponse(data)
+        const response_data = BaseResponse(200,'Data Found',data)
 
         res.json(response_data)
     } catch (error) {
@@ -27,7 +27,7 @@ const getOne = async (req, res) => {
         if (data === null) {
             return res.status(400).json({ code: 400, message: 'Data Not Found' })
         }
-        const response_data = BaseResponse(data)
+        const response_data = BaseResponse(200,'Data Found',data)
         res.json(response_data)
     } catch (error) {
         console.log(error.message)
@@ -41,7 +41,7 @@ const createData = async (req, res) => {
         }
 
         const data = await Areas.create(req.body)
-        const response_data = BaseResponse(data)
+        const response_data = BaseResponse(201,'Success Create',data)
         res.json(response_data)
     } catch (error) {
         console.log(error.message)
@@ -55,7 +55,7 @@ const updateData = async (req, res) => {
                 id: req.params.id
             }
         })
-        const response_data = BaseResponse(data)
+        const response_data = BaseResponse(201,'Success Update',data)
         res.json(response_data)
     } catch (error) {
         console.log(error.message)
@@ -67,7 +67,7 @@ const deleteData = async (req, res) => {
         const data = await Areas.destroy({
             id: req.params.id
         })
-        const response_data = BaseResponse(data)
+        const response_data = BaseResponse(201,'Success Delete',data)
         res.json(response_data)
     } catch (error) {
         console.log(error.message)
